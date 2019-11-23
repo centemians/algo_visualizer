1ccabfa5abf557d00c1c7d032feca74892d23bbf
Shakya <abshakya@deloitte.com>
2019-11-23 12:59:30 +0530

    Optimized for loop code

--- script.R
+++ script.R
@@ -2,19 +2,20 @@
 data <- data.frame("time" = c(rep(0,35)), "index" = c(1:35), "value" = c(90,10,0,3,1,56,32,3,54,98,34,53,24,17,84,38,76,9,38,76,10,38,64,39,87,34,25,55,34,22,13,57,1,2,3))
 
 size = 35
-data_array <- array(c(90,10,0,3,1,56,32,3,54,98,34,53,24,17,84,38,76,9,38,76,10,38,64,39,87,34,25,55,34,22,13,57,1,2,3))
+#data_array <- array(c(90,10,0,3,1,56,32,3,54,98,34,53,24,17,84,38,76,9,38,76,10,38,64,39,87,34,25,55,34,22,13,57,1,2,3))
 time_passed = 1
+
+df <- data.frame("time" = c(rep(1,35)), "index" = c(1:35), "value" = c(90,10,0,3,1,56,32,3,54,98,34,53,24,17,84,38,76,9,38,76,10,38,64,39,87,34,25,55,34,22,13,57,1,2,3))
 
 for(i in 1:size){
   for(k in 1:size-1){
-    if(any(data_array[k] > data_array[k+1])){
-      temp = data_array[k]
-      data_array[k] = data_array[k+1]
-      data_array[k+1] = temp
+    if(any(df[k,3] > df[k+1,3])){
+      temp = df[k,3]
+      df[k,3] = df[k+1,3]
+      df[k+1,3] = temp
     }
-    for(l in 1:size){
-      data <- rbind(data,list(time_passed,l,data_array[l]))
-    }
+    df$time = time_passed
+    data <- rbind(data,df)
     time_passed = time_passed + 1
   }
   
@@ -24,8 +25,6 @@ p_temp <-ggplot(data=data, aes(x=index, y=value, fill = index)) +
   geom_bar(stat="identity")
 
 anim <- p_temp + gganimate::transition_states(time,transition_length = 1,state_length = 1)
-
-anim1 <- p_temp + gganimate::transition_time(time)
 
 movie3 <- animate(anim, renderer = ffmpeg_renderer(format = ".gif"))
 b <- animate(anim,renderer = gifski_renderer(),nframes = 2000,fps=40)
